36fadfa55a590c6aecbdf0e0d164cd40656ca74b
Viacheslav Zhulov <slaaavyn@gmail.com>
2020-06-17 19:47:37 +0300

    init

---
+++ src/test/java/tk/slaaavyn/soft/industry/banking/BankingApplicationTests.java
@@ -0,0 +1,13 @@
+package tk.slaaavyn.soft.industry.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
